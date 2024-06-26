213e696a5a8c78965638a9cd8244bf210d73413d
Evvaog <evvaog@gmail.com>
2024-06-26 15:24:11 +0200

    Added filter (Type & country) for blogs

--- src/components/BlocksList.jsx
+++ src/components/BlocksList.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import blocks from "../db.json";
 import BlockItem from "./BlockItem";
+import Filter from "./Filter";
 
 const BlocksList = ({userRole}) => {
+    const [selectedTyp, setSelectedTyp] = useState("all");
+    const [selectedLand, setSelectedLand] = useState("all");
+
+    function setSelectedTypHandler(value) {
+        setSelectedTyp(value);
+    }
+
+    function setSelectedLandHandler(value) {
+        setSelectedLand(value);
+    }
+
+
+
     return (
         <div>
             <div className="blocks-container">
-                {blocks.map((b, index) => (
+                <Filter
+                    setSelectedTypHandler={setSelectedTypHandler}
+                    setSelectedLandHandler={setSelectedLandHandler}
+                />
+                {blocks.filter((blog) => selectedTyp === "all" ? blog !== null : blog.blockReiseTyp === selectedTyp).filter((blog) => selectedLand === "all"? blog !== null : blog.blockLand === selectedLand).map((b, index) => (
                     <div key={b.blockId}>
                         <BlockItem
                             userRole={userRole}
